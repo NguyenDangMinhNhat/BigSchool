93ae4b850ff304aa49a8e24d1a8f64001aca0f39
Nhat Nguyen <nnht133@gmail.com>
2022-03-28 11:21:54 +0700

    Buoi 5

--- BigSchool/Controllers/AttendancesController.cs
+++ BigSchool/Controllers/AttendancesController.cs
@@ -5,9 +5,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using System.Web.Mvc;
-using System.Web.Providers.Entities;
-using System.Web.Routing;
+using Microsoft.AspNet.Identity;
 
 namespace BigSchool.Controllers
 {
@@ -17,6 +15,21 @@ namespace BigSchool.Controllers
         public AttendancesController()
         {
             _dbContext = new ApplicationDbContext();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Attend([FromBody] int courseId)
+        {
+            var attendance = new Attendance
+            {
+                CourseId = courseId,
+                AttendeeId = User.Identity.GetUserId()
+            };
+
+            _dbContext.Attendances.Add(attendance);
+            _dbContext.SaveChanges();
+
+            return Ok();
         }
     }
 }
